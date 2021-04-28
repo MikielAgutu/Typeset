35451a6f0b32e42307e2bc849bcaf219c8d51b66
MikielAgutu <mikielagutu@yahoo.co.uk>
2021-04-28 20:32:23 +0100

    Remove '.js' for better GitHub language detection

--- Typeset/Typeset/StringResources.cs
+++ Typeset/Typeset/StringResources.cs
@@ -7,6 +7,6 @@
         public const string PageMarginalsCss = "pageMarginals.css";
         public const string PageNumberCss = "pageNumber.css";
         public const string DocumentHtml = "document.html";
-        public const string PagedJs = "paged.js";
+        public const string PagedJs = "pagedjs";
     }
 }
